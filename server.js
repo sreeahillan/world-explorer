2d4c776881963f0d39d1be5a1756b35ec8ffdea3
Sree ahillan <sreeahillan2@gmail.com>
2026-07-30 05:01:15 +0530

    Just Moved to the CSS part

--- server.js
+++ server.js
@@ -1,26 +1,51 @@
 
 import express from "express";
 import axios from "axios";
+import body from 'body-parser';
 import 'dotenv/config';
 
 
 const app = express();
 const port = 3000;
-let url = "https://api.restcountries.com"
+
+app.use(express.static("public"))
+app.use(body.urlencoded({extended:true}));
+
+const url = "https://api.restcountries.com"
 const API_KEY = process.env.API_KEY;
-let config =  {headers: { 'Authorization': `Bearer ${API_KEY}` }}
+const config =  {headers: { 'Authorization': `Bearer ${API_KEY}` }}
 
 
 app.get("/" , (req , res) => {
-    res.send("world explorer is running")
-    console.log(API_KEY);
+    res.render("index.ejs");
 });
 
-app.get("/joke" , async (req , res) => {
-    const country = req.query.country;
-    const result = await axios.get(url + `/countries/v5/names.common/${country}` , config);
-    console.log(result.data);
-    
+app.post("/joke", async (req , res) => {
+    try {
+        const country_name = req.body.input
+        const result = await axios.get(url + `/countries/v5/names.common/${country_name}` , config)
+        const country_data = {
+            country_common_name : result.data.data.objects[0].names.common,
+            country_capital_name : result.data.data.objects[0].capitals[0].name,
+            country_region_name : result.data.data.objects[0].region,
+            country_population : result.data.data.objects[0].population,
+            country_currency_name : result.data.data.objects[0].currencies[0].name,
+            country_language_names : result.data.data.objects[0].languages.map(obj => obj.name),
+            country_continent_name : result.data.data.objects[0].continents,
+            country_callingcode : result.data.data.objects[0].calling_codes,
+            country_flag : result.data.data.objects[0].flag.url_png,
+            country_driving_side : result.data.data.objects[0].cars.driving_side,
+        }
+        res.render("index.ejs" , {
+        data : country_data
+    })
+    } catch (error) {
+        console.log(error.message);
+        
+        res.render("index.ejs" , {
+        error_displayed : "Type the name correctly"
+    });
+}
 });
 
 app.listen(port , () => {
